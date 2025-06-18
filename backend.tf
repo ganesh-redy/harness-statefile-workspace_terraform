26d9130b264ad38f64c1fe6a26e65358c80582cc
ganesh-redy <ganeshreddy7610@gmail.com>
2025-06-18 18:42:12 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "http" {
-    address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/SFTY_Training/workspaces/IACMmigrateganesh/terraform-backend?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/irshaq/workspaces/man/terraform-backend?accountIdentifier=347FmVsmR6mi4o_O8pTfug"
     username = "harness"
-    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/SFTY_Training/workspaces/IACMmigrateganesh/terraform-backend/lock?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    lock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/irshaq/workspaces/man/terraform-backend/lock?accountIdentifier=347FmVsmR6mi4o_O8pTfug"
     lock_method = "POST"
-    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/SFTY_Training/workspaces/IACMmigrateganesh/terraform-backend/lock?accountIdentifier=ucHySz2jQKKWQweZdXyCog"
+    unlock_address = "https://app.harness.io/gateway/iacm/api/orgs/default/projects/irshaq/workspaces/man/terraform-backend/lock?accountIdentifier=347FmVsmR6mi4o_O8pTfug"
     unlock_method = "DELETE"
   }
 }
